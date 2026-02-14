2bd44ed935ffb01665471e3ffa35942e484dd024
Minsheng Liu <lambda@liu.ms>
2026-02-14 13:17:03 +0800

    Fix escaping closure in WuhuPerceptionTracking (#50)
    * Fix escaping closure in perception tracking wrapper
    
    * Fix SwiftFormat opaque generic parameter lint

--- WuhuApp/Sources/PerceptionTrackingCompat.swift
+++ WuhuApp/Sources/PerceptionTrackingCompat.swift
@@ -2,7 +2,7 @@ import ComposableArchitecture
 import SwiftUI
 
 @ViewBuilder
-func WuhuPerceptionTracking(@ViewBuilder _ content: () -> some View) -> some View {
+func WuhuPerceptionTracking(@ViewBuilder _ content: @escaping () -> some View) -> some View {
   #if os(macOS)
     content()
   #else
